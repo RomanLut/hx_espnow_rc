49d3dfcf15962e592105cea506cd374393defb25
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2021-09-17 18:04:49 +0300

    changed telemetry baudrate to 19200 in sbus receiver

--- examples/esp01_rx_sbus/include/rx_config.h
+++ examples/esp01_rx_sbus/include/rx_config.h
@@ -18,6 +18,7 @@ https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/
 //inverted SBUS = true ===> normal UART
 #define SBUS_INVERTED  false
 
+//#define TELEMETRY_BAUDRATE 115200
 #define TELEMETRY_BAUDRATE 19200
 
 //=============================================================================
