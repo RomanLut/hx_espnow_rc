4effb36e23aedcc7140c9116e359da12a26a1918
Nicholas Kruzan <nickkruzan@gmail.com>
2021-11-16 09:54:26 -0600

    create copy of esp01_rx_sbus as esp01_rx_ppm

---
+++ examples/esp01_rx_ppm/include/rx_config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/* ESP-01
+            ____________
+           |  -.-._.   |  
+           |           |
+           | **  ****  |
+           | **  ****  |
+           |     ****  |
+           |  o o o o  |         GND   GPIO2   GPIO1   RX      
+           |__o o o o__|         TX    CH_EN   RESET   VCC 
+
+https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/ 
+
+*/
+
+//output inverted SBUS? 
+//inverted SBUS = true ===> normal UART
+#define SBUS_INVERTED  false
+
+//#define TELEMETRY_BAUDRATE 115200
+#define TELEMETRY_BAUDRATE 9600
+
+//=============================================================================
+//Receiver binding
+#define USE_WIFI_CHANNEL 3
+#define USE_KEY 0 
