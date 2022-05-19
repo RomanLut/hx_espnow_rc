ccac147940e28dfe1bc65cf086bd3a22717148d3
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2022-05-19 03:11:22 +0300

    refactoring

--- lib/hx_rc_transmitter_common/hx_channels.h
+++ lib/hx_rc_transmitter_common/hx_channels.h
@@ -20,7 +20,7 @@ public:
         Serial.print("Failsafe: ");
         Serial.println(this->isFailsafe?1: 0);
 
-        for ( int i = 0; i < 16; i++ ) 
+        for ( int i = 0; i < HXRC_CHANNELS_COUNT; i++ ) 
         {
             Serial.print("Channel");
             Serial.print(i);
