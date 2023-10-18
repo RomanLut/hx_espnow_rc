0176026479c6b72d24e99cf618c474d9d01550aa
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-10-19 00:54:39 +0200

    implemented SBUS to USB Serial receiver

---
+++ examples/rx_d1_mini_usbserial_sbus/include/rx_config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+/* Wemos D1 Mini
+            _________
+           |  -.-._. |  
+     /RST =o         o= TX GPIO1
+     ADC0 =o         o= RX GPIO3
+GPIO16 D0 =o         o= D1 GPIO5
+GPIO14 D5 =o         o= D2 GPIO4
+GPIO12 D6 =o         o= D3 GPIO0
+GPIO13 D7 =o         o= D4 GPIO2
+GPIO15 D8 =o         o= GND
+     3.3V =o         o= 5V
+           |___USB___| 
+
+https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/ 
+
+Receiver which outputs SBUS protocol packets on USB Serial.
+To be used with simulators which expect SBUS receiver to be connected to COM port on PC, f.e. Inav SITL.
+
+*/
+
+#define SERIAL_BAUDRATE 100000
+
+//=============================================================================
+//Receiver binding
+#define USE_WIFI_CHANNEL 3
+#define USE_KEY 0 
---
+++ examples/rx_d1_mini_usbserial_sbus/src/main.cpp
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include "HX_ESPNOW_RC_Slave.h"
+#include "rx_config.h"
+#include "hx_sbus_encoder.h"
+#include "HX_ESPNOW_RC_SerialBuffer.h"
+
+#include <ArduinoOTA.h>
+
+HXRCSlave hxrcSlave;
+HXRCSerialBuffer<512> hxrcTelemetrySerial( &hxrcSlave );
+HXSBUSEncoder hxSBUSEncoder;
+
+unsigned long lastStats = millis();
+
+//=====================================================================
+//=====================================================================
+void processIncomingTelemetry()
+{
+  while ( hxrcTelemetrySerial.getAvailable() > 0 && Serial.availableForWrite() > 0)
+  {
+    uint8_t c = hxrcTelemetrySerial.read();
+  }
+}
+
+
+//=====================================================================
+//=====================================================================
+void setup()
+{
+  hxSBUSEncoder.init( Serial, 1, true );
+
+  hxrcSlave.init(
+      HXRCConfig(
+          USE_WIFI_CHANNEL,
+          USE_KEY,
+          false,
+          -1, false));
+
+  //REVIEW: receiver does not work if AP is not initialized?
+  WiFi.softAP("hxrcrusbsbus", NULL, USE_WIFI_CHANNEL);
+
+  ArduinoOTA.begin();  
+}
+
+//=====================================================================
+//=====================================================================
+void updateSBUSOutput()
+{
+  //set failsafe flag
+  bool failsafe = hxrcSlave.getReceiverStats().isFailsafe();
+  hxSBUSEncoder.setFailsafe( failsafe);
+  
+  //inject RSSI into channel 16
+  hxSBUSEncoder.setChannelValue( HXRC_CHANNELS_COUNT-1, 1000 + ((uint16_t)hxrcSlave.getReceiverStats().getRSSI())*10 );
+
+  if ( !failsafe ) //keep last channel values on failsafe
+  {
+    HXRCChannels channels = hxrcSlave.getChannels();
+    for ( int i = 0; i < HXRC_CHANNELS_COUNT-1; i++)
+    {
+      hxSBUSEncoder.setChannelValue( i, channels.getChannelValue(i) );
+    }
+  }
+
+  hxSBUSEncoder.loop( Serial );
+}
+
+//=====================================================================
+//=====================================================================
+void loop()
+{
+  hxrcTelemetrySerial.flushIn();
+  processIncomingTelemetry();
+
+  hxrcSlave.loop();
+
+  updateSBUSOutput();
+
+  if ( hxrcSlave.getReceiverStats().isFailsafe() )
+  {
+    ArduinoOTA.handle();  
+  }
+}
